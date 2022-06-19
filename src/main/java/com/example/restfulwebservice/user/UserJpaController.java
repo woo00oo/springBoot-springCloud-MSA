a0d2f11f14f3a431a8bb7c1cea9d3bd039a796cd
woo00oo <96woo94@naver.com>
2022-06-19 14:47:44 +0900

    JPA Service 구현을 위한 Controller, Repository 생성

---
+++ src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -0,0 +1,22 @@
+package com.example.restfulwebservice.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/jpa")
+public class UserJpaController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userRepository.findAll();
+    }
+}
