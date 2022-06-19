2091684280ebec07da92d78f6be811c81ec822ae
woo00oo <96woo94@naver.com>
2022-06-19 11:31:34 +0900

    Spring data jpa를 이용한 Entity 설정과 초기 데이터 생성

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -19,11 +20,17 @@ import java.util.Date;
 //@JsonIgnoreProperties(value = {"password", "ssn"})
 //@JsonFilter("UserInfo")
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
     @ApiModelProperty(notes = "사용자 이름을 입력해 주세요")
     private String name;
+
     @Past
     @ApiModelProperty(notes = "사용자 등록일을 입력해 주세요")
     private Date joinDate;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users values(1, now(), 'User1', 'test1111', '1234-1234');
+insert into users values(2, now(), 'User2', 'test2222', '5678-1234');
+insert into users values(3, now(), 'User3', 'test3333', '1234-5678');
